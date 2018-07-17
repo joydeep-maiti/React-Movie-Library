0caa9c099f8fff17c3f51e54c83ad89c9ccfcacc
Joydeep Maiti <joydeep127@gmail.com>
2018-07-17 16:19:03 +0530

    Active class added

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ class App extends Component {
   state = {
     all: ['English','Hindi', 'Bengali', 'Tamil', 'Telugu'],
     language: 'Hindi',
+    active: 'Hindi'
   }
 
   changehandler = (index) => {
     const allLanguage = this.state.all;
     this.setState({
-      language: allLanguage[index]
+      language: allLanguage[index],
+      active: allLanguage[index]
     })
   }
   render() {
@@ -26,7 +28,7 @@ class App extends Component {
         <main className={classes.main}>
           <div className={classes.sidebar}>
             {this.state.all.map((lang, index) => {
-              return <Item key={lang} clicked={() => this.changehandler(index)} title={lang} />
+              return <Item  key={lang} clicked={() => this.changehandler(index)} title={lang} active={this.state.active}/>
             })}
           </div>  
           <Moviecards language={this.state.language}/>
--- src/Components/Item/Item.js
+++ src/Components/Item/Item.js
@@ -2,9 +2,14 @@ import React from 'react';
 import classes from './Item.css';
 
 const item = (props) => {
-    return (
-            <p className={classes.items} onClick={props.clicked}>{props.title}</p>
-    );
+    let comp = null;
+    if(props.active === props.title) {
+        comp = <p className={classes.itemactive} onClick={props.clicked}>{props.title}</p>
+    }
+    else {
+        comp = <p className={classes.items} onClick={props.clicked}>{props.title}</p>
+    }
+    return (comp);
 }
 
 export default item;
