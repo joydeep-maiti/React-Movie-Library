57b5de1f8d904ed481bb292fa61924efee96087f
Joydeep Maiti <joydeep127@gmail.com>
2018-07-25 20:18:05 +0530

    adding navlink

--- src/App.js
+++ src/App.js
@@ -5,30 +5,34 @@ import Moviecards from './Container/Moviecards/Moviecards';
 import classes from './App.css';
 import Item from './Components/Item/Item';
 import { connect } from 'react-redux';
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
 
 class App extends Component {
 
-  changehandler = (index) => {
-    const allLanguage = this.state.all;
-    this.setState({
-      active: allLanguage[index]
-    })
-  }
   render() {
     return (
-      <div className={classes.App}>
-        <header>
-          <img src={logo} alt="Movielogo" className={classes.logo}/>
-        </header>
-        <main className={classes.main}>
-          <div className={classes.sidebar}>
-            {this.props.all.map((lang, index) => {
-              return <Item  key={lang} clicked={()=>this.props.changehandler(lang)} title={lang} active={this.props.active}/>
-            })}
-          </div>  
-          <Moviecards language={this.props.active}/>
-        </main>    
-      </div>
+      <BrowserRouter>
+        <div className={classes.App}>
+          <header>
+            <img src={logo} alt="Movielogo" className={classes.logo}/>
+            <h2> <NavLink to='/'>Home</NavLink> </h2>
+            <h2> <NavLink to='/web'>web</NavLink> </h2>
+          </header>
+          <main className={classes.main}>
+            <div className={classes.sidebar}>
+              {this.props.all.map((lang, index) => {
+                return <Item  key={lang} clicked={()=>this.props.changehandler(lang)} title={lang} active={this.props.active}/>
+              })}
+            </div>  
+            <Moviecards language={this.props.active}/>
+            <div>
+              <Route exact path="/" render={() => <div>Home</div>} />
+              <Route path="/web" render={() => <div>Web</div>} />
+            </div>
+          </main>    
+        </div>
+        
+      </BrowserRouter>
     );
   }
 }
