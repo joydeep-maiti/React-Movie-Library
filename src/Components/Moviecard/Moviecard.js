2df00ad020aa19b0be0d241ea90be029800a9dc6
Joydeep Maiti <joydeep127@gmail.com>
2018-07-27 19:36:28 +0530

    making project responsive

--- src/Components/Moviecard/Moviecard.js
+++ src/Components/Moviecard/Moviecard.js
@@ -3,34 +3,9 @@ import classes from './Moviecard.css';
 
 
 const moviecard = (props) => {
-    // let card = null;
-    // const lang = props.language;
-    // switch (lang) {
-    //     case 'English':
-    //         card = <div className={classes.English} onClick={props.clicked}></div>
-    //         break;
-        
-    //     case 'Hindi':
-    //         card = <img src={props.url} className={classes.Hindi} onClick={props.clicked}/>
-    //         break;
-
-    //     case 'Bengali':
-    //         card = <div className={classes.Bengali} onClick={props.clicked}></div>
-    //         break;
-
-    //     case 'Tamil':
-    //         card = <div className={classes.Tamil} onClick={props.clicked}></div>
-    //         break;
-
-    //     case 'Telugu':
-    //         card = <div className={classes.Telugu} onClick={props.clicked}></div>
-    //         break;
-    //     default:
-    //         card = <div className={classes.Hindi} onClick={props.clicked}></div>
-    //         break;
-    // }
-
-    return (<img src={props.url} className={classes.Hindi} onClick={props.clicked} />);
+    return (
+        <img src={props.url} className={classes.card} onClick={props.clicked} />
+    );
 }
 
 export default moviecard;
